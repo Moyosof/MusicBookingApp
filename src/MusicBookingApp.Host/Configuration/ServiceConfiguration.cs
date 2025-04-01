cc6abd7485264b32031cbdc4dd16eef55c2e3264
Shittu Taiwo <101399654+Moyosof@users.noreply.github.com>
2025-04-01 10:15:45 +0100

    add rate limit and add dockerfile to root folder (#3)

--- src/MusicBookingApp.Host/Configuration/ServiceConfiguration.cs
+++ src/MusicBookingApp.Host/Configuration/ServiceConfiguration.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 using MusicBookingApp.Application.Contracts;
@@ -36,6 +37,29 @@ namespace MusicBookingApp.Host.Configuration
                 .AsPublicImplementedInterfaces();
 
             return services;
+        }
+
+        public static void ConfigureRateLimiter(this IServiceCollection services)
+        {
+            services.AddRateLimiter(options =>
+            {
+                // Fixed Window Limiter (Allows 3 requests per 10 seconds)
+                options.AddFixedWindowLimiter("Fixed", opt =>
+                {
+                    opt.Window = TimeSpan.FromSeconds(10);
+                    opt.PermitLimit = 3;
+                });
+
+                // Sliding Window Limiter (Allows 10 requests per minute)
+                options.AddSlidingWindowLimiter("SlidingWindow", opt =>
+                {
+                    opt.PermitLimit = 10;
+                    opt.Window = TimeSpan.FromMinutes(1);
+                    opt.SegmentsPerWindow = 4; // Each segment represents 15 seconds
+                });
+
+                options.RejectionStatusCode = 429;
+            });
         }
 
         public static IServiceCollection RegisterApplicationRepository<T>(
